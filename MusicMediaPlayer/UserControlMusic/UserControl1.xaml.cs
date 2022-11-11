cb972c5d36632380938f46f8eecc0867e21efbc0
Dang Thai Son <sondanguit@gmail.com>
2022-11-11 11:42:47 +0700

    baseProject

---
+++ MusicMediaPlayer/UserControlMusic/UserControl1.xaml.cs
@@ -0,0 +1,37 @@
+﻿using MusicMediaPlayer.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MusicMediaPlayer.UserControlMusic
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControlViewModel ViewModel { get; set; }
+        public UserControl1()
+        {
+            InitializeComponent();
+            ViewModel = new UserControlViewModel();
+            this.DataContext = ViewModel;
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
