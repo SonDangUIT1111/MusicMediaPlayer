8a3c06bc9e38566c683147c15cb0b03fc2c769c2
280403 <kietphamkb2@gmail.com>
2022-12-24 22:02:53 +0700

    Điều chỉnh một số thư mục để merge code

---
+++ MusicMediaPlayer/ViewModel/AddSongPlayListViewModel.cs
@@ -0,0 +1,74 @@
+﻿using MusicMediaPlayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace MusicMediaPlayer.ViewModel
+{
+    public class AddSongPlayListViewModel : BaseViewModel
+    {
+        #region commands
+        public ICommand Loaded { get; set; }
+        public ICommand Add { get; set; }
+        public ICommand Cancel { get; set; }
+
+        #endregion
+
+        private ObservableCollection<MusicMediaPlayer.Model.Song> _List;
+        public ObservableCollection<MusicMediaPlayer.Model.Song> List { get => _List; set { _List = value; OnPropertyChanged(); } }
+
+        public MusicMediaPlayer.Model.PlayList pl;
+
+        AddSongPlayList thiswindow;
+
+        public AddSongPlayListViewModel()
+        {
+            Loaded = new RelayCommand<Window>((p) => { return true; }, (p) =>
+            {
+                thiswindow = p as AddSongPlayList;
+                LoadDanhSach();
+            });
+
+            Add = new RelayCommand<object>((p) =>
+            {
+                if (thiswindow != null && thiswindow.listview.SelectedItems.Count != 0)
+                    return true;
+                return false;
+            }, (p) =>
+            {
+                foreach (Song item in thiswindow.listview.SelectedItems)
+                {
+                    item.PlayList.Add(pl);
+                    pl.Song.Add(item);
+                    pl.SongCount = pl.SongCount + 1;
+                }
+
+                DataProvider.Ints.DB.SaveChanges();
+                thiswindow.Close();
+            });
+
+            Cancel = new RelayCommand<Window>((p) => { return true; }, (p) =>
+            {
+                thiswindow.Close();
+            });
+        }
+
+        void LoadDanhSach()
+        {
+            ObservableCollection<MusicMediaPlayer.Model.Song> list = new ObservableCollection<MusicMediaPlayer.Model.Song>(DataProvider.Ints.DB.Song);
+
+            foreach (Song item in pl.Song)
+            {
+                list.Remove(item);
+            }
+
+            List = list;
+        }
+    }
+}
