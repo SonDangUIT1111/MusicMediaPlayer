416f9ccc60287efc0c44089c129f523486919521
Dang Thai Son <sondanguit@gmail.com>
2022-12-27 22:20:15 +0700

    ChangeDatabase

--- MusicMediaPlayer/Model/PlayList.cs
+++ MusicMediaPlayer/Model/PlayList.cs
@@ -25,6 +25,7 @@ namespace MusicMediaPlayer.Model
         public Nullable<int> SongCount { get; set; }
         public int OwnerId { get; set; }
         public byte[] ImagePlaylistBinary { get; set; }
+        public Nullable<System.DateTime> TimeCreate { get; set; }
     
         public virtual UserAccount UserAccount { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
