e4552daf9c40cd7639c0d76997a6f6f50400954e
280403 <kietphamkb2@gmail.com>
2023-01-13 14:21:19 +0700

    MessageBoxYesNo

---
+++ MusicMediaPlayer/ViewModel/MessageBoxYesNoViewModel.cs
@@ -0,0 +1,49 @@
+﻿using MusicMediaPlayer.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MusicMediaPlayer.ViewModel
+{
+    public class MessageBoxYesNoViewModel : BaseViewModel
+    {
+        #region commands
+        public ICommand Yes { get; set; }
+
+        public ICommand No { get; set; }
+
+        #endregion
+
+        private string _Title;
+        public string Title { get => _Title; set { _Title = value; OnPropertyChanged(); } }
+
+        private string _Question;
+        public string Question { get => _Question; set { _Question = value; OnPropertyChanged(); } }
+
+        public bool IsYes = false;
+        public MessageBoxYesNoViewModel()
+        {
+            Title = "Exit";
+            Question = "Do you want to exit?";
+
+            No = new RelayCommand<Window>((p) => { return true; }, (p) =>
+            {
+                p.Close();
+            }
+            );
+
+            Yes = new RelayCommand<Window>((p) => { return true; }, (p) =>
+            {
+                IsYes = true;
+                p.Close();
+            }
+            );
+        }
+
+    }
+}
